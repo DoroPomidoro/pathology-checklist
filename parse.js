9db68876ad1fc5bd7a0dba79a2c9fc318387f264
Uladzislau <94238251+DoroPomidoro@users.noreply.github.com>
2023-05-29 21:10:51 +0000

    feat: add comments with Danila

--- parse.js
+++ parse.js
@@ -11,33 +11,32 @@
 //     }
 //     recursy(form);
 // })
-
+let finalReport = {};
+let collectFields = document.querySelectorAll('fieldset');
 function getTitle() {
     let arr = [];
-    let count = document.querySelectorAll('fieldset').length;
+    let count = collectFields.length;
     for (let i = 0; i < count; i++) {
-        arr.push(document.querySelectorAll('fieldset')[i].children[0].innerText)
-        // console.log(document.querySelectorAll('fieldset')[i].children[0].innerText)
+        arr.push(collectFields[i].children[0].innerText)
+        finalReport[`${collectFields[i].children[0].innerText}`] = 'answer';
     }
-    console.log(arr);
 }
 
 document.querySelector('#btn-final').addEventListener('click', (event) => {
     event.preventDefault();
-
-    let collectionCheckedAnswer = [];
-    let collectFieldts = document.querySelectorAll('fieldset');
-    let collectCount = document.querySelectorAll('fieldset').length;
+    getTitle();
+    let collectionCheckedAnswer = {};
+    
+    let collectCount = collectFields.length;
     for (let i = 0; i < collectCount; i++) {
-        
-        let count = collectFieldts[i].elements.length;
+        let name =  collectFields[i].children[0].innerText
+        let count = collectFields[i].elements.length;
         for (let j = 0; j < count; j++) {
-            let searchChecked = collectFieldts[i].elements[j];
+            let searchChecked = collectFields[i].elements[j];
             if (searchChecked.checked === true) {
-                collectionCheckedAnswer.push(searchChecked.value);
+                collectionCheckedAnswer[`${name}`] = searchChecked.value;
             }
         }   
     }
     console.log(collectionCheckedAnswer);
-    getTitle();
 })
