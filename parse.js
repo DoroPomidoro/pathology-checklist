378dd3303a66c13b86ac5fb5533b02e6fa6edea9
Uladzislau <94238251+DoroPomidoro@users.noreply.github.com>
2023-05-29 20:08:49 +0000

    feat: add JS functions

--- parse.js
+++ parse.js
@@ -12,26 +12,32 @@
 //     recursy(form);
 // })
 
-document.querySelector('#btn-final').addEventListener('click', () => {
-    console.log(document.querySelector('#endoscopic-resection').value);
-    let myCheckBox = document.querySelector('#endoscopic-resection');
-    
-    console.log(myCheckBox.checked);
-    console.log(myCheckBox.value);
-    // if (myCheckBox.checked) {
-
-    // } else {
-
-    // }
-
-})
-
-let procedure-type = document.querySelector('.procedure-type');
+function getTitle() {
+    let arr = [];
+    let count = document.querySelectorAll('fieldset').length;
+    for (let i = 0; i < count; i++) {
+        arr.push(document.querySelectorAll('fieldset')[i].children[0].innerText)
+        // console.log(document.querySelectorAll('fieldset')[i].children[0].innerText)
+    }
+    console.log(arr);
+}
 
 document.querySelector('#btn-final').addEventListener('click', (event) => {
     event.preventDefault();
 
-    let form = document.querySelector('form');
-    // console.log(form);
-    console.log(form.elements.other.value);
-});
+    let collectionCheckedAnswer = [];
+    let collectFieldts = document.querySelectorAll('fieldset');
+    let collectCount = document.querySelectorAll('fieldset').length;
+    for (let i = 0; i < collectCount; i++) {
+        
+        let count = collectFieldts[i].elements.length;
+        for (let j = 0; j < count; j++) {
+            let searchChecked = collectFieldts[i].elements[j];
+            if (searchChecked.checked === true) {
+                collectionCheckedAnswer.push(searchChecked.value);
+            }
+        }   
+    }
+    console.log(collectionCheckedAnswer);
+    getTitle();
+})
