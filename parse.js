b10e1b380d8c3ac77453c6c502a9663e9188673b
Uladzislau <94238251+DoroPomidoro@users.noreply.github.com>
2023-06-01 04:25:01 +0000

    feat: add serialize

--- parse.js
+++ parse.js
@@ -11,32 +11,40 @@
 //     }
 //     recursy(form);
 // })
-let finalReport = {};
-let collectFields = document.querySelectorAll('fieldset');
-function getTitle() {
-    let arr = [];
-    let count = collectFields.length;
-    for (let i = 0; i < count; i++) {
-        arr.push(collectFields[i].children[0].innerText)
-        finalReport[`${collectFields[i].children[0].innerText}`] = 'answer';
-    }
-}
+
+
+
+// let finalReport = {};
+// let collectFields = document.querySelectorAll('fieldset');
+// function getTitle() {
+//     let arr = [];
+//     let count = collectFields.length;
+//     for (let i = 0; i < count; i++) {
+//         arr.push(collectFields[i].children[0].innerText)
+//         finalReport[`${collectFields[i].children[0].innerText}`] = 'answer';
+//     }
+// }
+
+// document.querySelector('#btn-final').addEventListener('click', (event) => {
+//     event.preventDefault();
+//     getTitle();
+//     let collectionCheckedAnswer = {};
+    
+//     let collectCount = collectFields.length;
+//     for (let i = 0; i < collectCount; i++) {
+//         let name =  collectFields[i].children[0].innerText;
+//         let count = collectFields[i].elements.length;
+//         for (let j = 0; j < count; j++) {
+//             let searchChecked = collectFields[i].elements[j];
+//             if (searchChecked.checked === true) {
+//                 collectionCheckedAnswer[`${name}`] = searchChecked.value;
+//             }
+//         }   
+//     }
+//     console.log(collectionCheckedAnswer);
+// })
 
 document.querySelector('#btn-final').addEventListener('click', (event) => {
     event.preventDefault();
-    getTitle();
-    let collectionCheckedAnswer = {};
-    
-    let collectCount = collectFields.length;
-    for (let i = 0; i < collectCount; i++) {
-        let name =  collectFields[i].children[0].innerText
-        let count = collectFields[i].elements.length;
-        for (let j = 0; j < count; j++) {
-            let searchChecked = collectFields[i].elements[j];
-            if (searchChecked.checked === true) {
-                collectionCheckedAnswer[`${name}`] = searchChecked.value;
-            }
-        }   
-    }
-    console.log(collectionCheckedAnswer);
+    console.log($("form").serialize())
 })
